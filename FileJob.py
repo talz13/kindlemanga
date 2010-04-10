3015b0b66a0dc8ad1d2f6a341565fbcd3c382f89
Jeff <Jeff@jeffDesktop>
2010-04-10 17:09:59 -0400

    Attempt to find series name from file's parent folder name

--- FileJob.py
+++ FileJob.py
@@ -1,14 +1,18 @@
 __author__="Jeff Byrom"
 __date__ ="$Apr 2, 2010 8:54:00 AM$"
 
+import os
+
 class FileJob:
 
     filename = ''
     seriesName = ''
-    volume = 0
+    volume = ''
 
     def __init__(self, filename):
         self.filename = filename
+        names = os.path.split(filename)
+        self.seriesName = names[0][names[0].rindex('\\') + 1:len(names[0])]
 
     def setSeriesName(self, seriesName):
         self.seriesName = seriesName
