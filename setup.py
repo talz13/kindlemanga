2511c64fe72cfa8bf604c869ab6ab6183802b283
Jeff <Jeff@jeffDesktop>
2010-04-11 12:51:13 -0400

    Removed gui/*.bmp from data_files args.

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ import sys
 from distutils.core import setup
 import py2exe
 
-data_files=[('.', glob.glob(sys.prefix + '/Lib/site-packages/UnRAR2/UnRARDLL/unrar.dll')),
-('gui', glob.glob('gui/*.bmp'))]
+data_files=[('.', glob.glob(sys.prefix + '/Lib/site-packages/UnRAR2/UnRARDLL/unrar.dll'))]
 options_bundle_python = dict(bundle_files = 1, dist_dir = './dist_full')
 options_bundle_depends = dict(bundle_files = 2, dist_dir = './dist_dep')
 options_bundle_none = dict(bundle_files = 3, dist_dir = './dist_none')
