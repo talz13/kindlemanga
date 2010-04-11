883d2734a55af5315cded951c750ee1ba37c2747
Jeff <Jeff@jeffDesktop>
2010-04-11 10:28:49 -0400

    Added .cbr and .cbz to file open dialog filter. Commented out debugging print statements. Added chapter support. Resize columns somewhat intelligently on init / add file / remove file, and after changing series name. Removed wxListBox control, only using wxListCtrl now. Removed OnSelectJob (from wxListBox usage), replaced with contents of OnSelectCtrlJob (from wxListCtrl usage) Added function to duplicate functionality of wxListBox.GetSelections() for wxListCtrl widget. Move up out of working directory after processing file so explorer doesn't complain about folder being in use.

--- kindlemanga.py
+++ kindlemanga.py
@@ -21,12 +21,19 @@ class MainFrame(KindleMangaLayout.KindleMangaFrame):
         sys.stderr = self.m_textCtrl_console
         
         self.m_textCtrl_outDir.SetValue(os.getenv('USERPROFILE') + '\\My Documents')
+        self.m_listCtrl1.InsertColumn(0, 'Filename')
+        self.m_listCtrl1.InsertColumn(1, 'Series')
+        self.m_listCtrl1.InsertColumn(2, 'Volume')
+        self.m_listCtrl1.InsertColumn(3, 'Chapter')
+        self.resizeCols()
+        #self.m_listCtrl1.SetColumnWidth(0, wx.LIST_AUTOSIZE)
+
 
     def OnOpen(self, event):
         self.dirname = ''
-        dlg = wx.FileDialog(self, "Choose file(s) to add to list", self.dirname, '', 'Rar, zip files (*.rar;*.zip)|*.rar;*.zip', wx.FLP_CHANGE_DIR|wx.FLP_FILE_MUST_EXIST|wx.FLP_OPEN|wx.FD_MULTIPLE)
+        dlg = wx.FileDialog(self, "Choose file(s) to add to list", self.dirname, '', 'Rar, zip files (*.rar;*.cbr;*.zip;*.cbz)|*.rar;*.cbr;*.zip;*.cbz', wx.FLP_CHANGE_DIR|wx.FLP_FILE_MUST_EXIST|wx.FLP_OPEN|wx.FD_MULTIPLE)
         if dlg.ShowModal() == wx.ID_OK:
-            print os.getcwd()
+            #print os.getcwd()
             self.filenames = dlg.GetFilenames()
             self.dirname = dlg.GetDirectory() + '\\'
             #print self.dirname
@@ -35,39 +42,72 @@ class MainFrame(KindleMangaLayout.KindleMangaFrame):
                 tempFilename = self.dirname + filename
                 if tempFilename.find('\\\\') > 0:
                     tempFilename = tempFilename.replace('\\\\','\\')
-                self.filenames[index] = tempFilename
+                #self.filenames[index] = tempFilename
+                self.filenames[index] = filename
                 # filename, data, size, datetime, dir=None
                 self.fileList.append(FileJob.FileJob(tempFilename))
-            print self.filenames
-            self.m_listBox1.InsertItems(self.filenames, self.m_listBox1.GetCount())
+                listIndex = self.m_listCtrl1.InsertStringItem(100, self.fileList[-1].getFilename())
+                self.m_listCtrl1.SetStringItem(listIndex, 1, self.fileList[-1].getSeriesName())
+                self.m_listCtrl1.SetStringItem(listIndex, 2, self.fileList[-1].getVolume())
+                self.m_listCtrl1.SetStringItem(listIndex, 3, self.fileList[-1].getChapter())
+            #print self.filenames
         dlg.Destroy()
+        self.resizeCols()
+        if (len(self.getSelections()) == 0):
+            if (self.m_listCtrl1.GetItemCount() != 0):
+                self.m_listCtrl1.Select(0)
+                self.OnSelectJob(event)
         return [self.dirname, self.filenames]
 
     def OnRemove(self, event):
-        self.selections = self.m_listBox1.GetSelections()
-        print self.selections
-        for index in self.selections:
+        selections = self.getSelections()
+        #print selections
+        for index in selections:
             del self.fileList[index]
-            self.m_listBox1.Delete(index)
+            self.m_listCtrl1.DeleteItem(index)
+        self.resizeCols()
 
     def OnProcess(self, event):
         self.ProcessAll(self.fileList, self.m_textCtrl_outDir.GetValue())
 
     def OnTextSeries(self, event):
         #print "OnTextSeries"
-        selected = self.m_listBox1.GetSelections()
+        selected = self.getSelections()
+        #print selected
         if len(selected) == 1:
             index = selected[0]
+            #index = selected
             self.fileList[index].setSeriesName(self.m_textCtrl_series.GetValue())
+            self.m_listCtrl1.SetStringItem(index, 1, self.fileList[index].getSeriesName())
             #print self.fileList[index].getSeriesName()
+        else:
+            for index in selected:
+                #print index, self.fileList[index].getSeriesName()
+                if (index < len(self.fileList)):
+                    self.fileList[index].setSeriesName(self.m_textCtrl_series.GetValue())
+                    self.m_listCtrl1.SetStringItem(index, 1, self.fileList[index].getSeriesName())
+        self.resizeCols()
+
     
     def OnTextVolume(self, event):
-        selected = self.m_listBox1.GetSelections()
-        if len(selected) == 1:
-            index = selected[0]
-            if (self.m_textCtrl_volume.GetValue().isdigit()):
-                self.fileList[index].setVolume(self.m_textCtrl_volume.GetValue())
-            #print self.fileList[index].getVolume()
+        selected = self.getSelections()
+        if (len(selected) == 1):
+            if len(selected) == 1:
+                index = selected[0]
+                if (self.m_textCtrl_volume.GetValue().isdigit()):
+                    #if (c for c in self.m_textCtrl_volume.GetValue() if c in '0123456789.'):
+                    self.fileList[index].setVolume(self.m_textCtrl_volume.GetValue())
+                    self.m_listCtrl1.SetStringItem(index, 2, str(self.fileList[index].getVolume()))
+
+    def OnTextChapter(self, event):
+        selected = self.getSelections()
+        if (len(selected) == 1):
+            if len(selected) == 1:
+                index = selected[0]
+                if (self.m_textCtrl_ch.GetValue().isdigit()):
+                    #if (c for c in self.m_textCtrl_volume.GetValue() if c in '0123456789.'):
+                    self.fileList[index].setChapter(self.m_textCtrl_ch.GetValue())
+                    self.m_listCtrl1.SetStringItem(index, 3, str(self.fileList[index].getChapter()))
 
     def OnOutDir(self, event):
         self.outDir = self.m_textCtrl_outDir.GetValue()
@@ -79,31 +119,74 @@ class MainFrame(KindleMangaLayout.KindleMangaFrame):
         else:
             self.dirname = os.getcwd()
 
-        print "Opening dir: " + self.dirname
+        #print "Opening dir: " + self.dirname
         dlg = wx.DirDialog(self, "Choose directory to create output files", self.dirname)
         if dlg.ShowModal() == wx.ID_OK:
-            print os.getcwd()
+            #print os.getcwd()
             self.dirname = dlg.GetPath()
-            print self.dirname
+            #print self.dirname
             self.m_textCtrl_outDir.SetValue(self.dirname)
         dlg.Destroy()
 
     def OnSelectJob(self, event):
-        selected = self.m_listBox1.GetSelections()
-        if len(selected) == 1:
+        selected = self.getSelections()
+
+        #if (self.m_listCtrl1.GetSelectedItemCount() == 1):
+        if (len(selected) == 1):
+            #selected = self.m_listCtrl1.GetNextItem(-1, state = wxLIST_STATE_SELECTED)
+            #selected = self.m_listCtrl1.GetFirstSelected()
             index = selected[0]
-            self.m_textCtrl_series.SetValue(self.fileList[index].getSeriesName())
-            self.m_textCtrl_volume.SetValue(str(self.fileList[index].getVolume()))
+            
+            #if (selected > -1):
+            if (index > -1):
+                seriesName = self.m_listCtrl1.GetItem(index, 1)
+                volume = self.m_listCtrl1.GetItem(index, 2)
+                ch = self.m_listCtrl1.GetItem(index, 3)
+                #print seriesName.GetText()
+                #print self.m_listCtrl1.GetItemText(item)
+                #print value
+                self.m_textCtrl_series.SetValue(seriesName.GetText())
+                self.m_textCtrl_volume.SetValue(volume.GetText())
+                self.m_textCtrl_ch.SetValue(ch.GetText())
+        #elif (self.m_listCtrl1.GetSelectedItemCount() > 1):
+        elif (len(selected) > 1):
+            #selected = self.m_listCtrl1.GetNextItem(-1, state = wx.LIST_STATE_SELECTED)
+            #while (selected >= 0):
+            for index in selected:
+                #print selected
+
+                if (len(self.fileList) > index):
+                    sameSeriesName = True
+                    firstSeriesName = self.fileList[index].getSeriesName()
+                    #for index in selected:
+                    #    # set the list ctrl to same selection:
+                    #    self.m_listCtrl1.SetItemState(index, wx.LIST_STATE_SELECTED, wx.LIST_STATE_SELECTED)
+                    #for index in selected:
+                    if (firstSeriesName != self.fileList[index].getSeriesName()):
+                        sameSeriesName = False
+                        break
+                    if (sameSeriesName):
+                        self.m_staticText_archiveName.SetLabel('')
+                        self.m_textCtrl_series.SetValue(firstSeriesName)
+                        self.m_textCtrl_volume.SetValue('')
+                        self.m_textCtrl_ch.SetValue('')
+
+                #selected = self.m_listCtrl1.GetNextItem(selected, state = wx.LIST_STATE_SELECTED)
+                #selected = self.m_listCtrl1.GetNextItem(selected, state = wx.LIST_STATE_SELECTED)
+                #print selected
+            #print 'multiple selected'
+
 
     def ProcessAll(self, fileJobList, outDir):
         unknownVol = 0;
         for job in fileJobList:
             filename = job.getFilename()
-            print 'filename: ' + filename
+            #print 'filename: ' + filename
 
             filepath = os.path.split(filename)
             print 'filepath: ' + str(filepath)
             volume = job.getVolume()
+            ch = job.getChapter()
             if (job.getSeriesName() != ''):
                 seriesName = job.getSeriesName()
             else:
@@ -111,6 +194,8 @@ class MainFrame(KindleMangaLayout.KindleMangaFrame):
                 unknownVol += 1
                 volume = unknownVol
             if (outDir != ''):
+                #if (not os.path.isdir(outDir)):
+                #    self.dialog_dir_not_found.show()
                 tempDirName = outDir
             else:
                 tempDirName = os.getenv('APPDATA') + '\\' + 'kindleMangaTemp'
@@ -118,12 +203,15 @@ class MainFrame(KindleMangaLayout.KindleMangaFrame):
             tempDir = tempDirName + '\\' # + seriesName
             print tempDir
             os.chdir(tempDir)
-            print os.getcwd()
+            #print os.getcwd()
             #if job.getSeriesName().len > 0:
             #
             #imageDir = os.path.splitext(filepath[1])[0]
             #tempImageDir = raw_input('set imageDir: [' + imageDir + ']: ')
-            tempImageDir = seriesName + '_' + ('%03d' % volume)
+            tempImageDir = seriesName.replace(' ', '_') + '-vol.' + ('%03d' % int(volume))
+            if (len(ch) > 0):
+                tempImageDir += '-ch.' + ('%03d' % int(ch))
+                
             if (len(tempImageDir) != 0):
                 imageDir = tempImageDir
 
@@ -148,10 +236,31 @@ class MainFrame(KindleMangaLayout.KindleMangaFrame):
 
             ProcessImages.ProcessAndSave(entries, targetHeight, maxWidth)
             KindleMangaFile.WriteMangaFiles()
+            os.chdir('..')
         print 'done'
 
     def printBox(self, text):
         self.m_textCtrl_console.AppendText(text)
+
+    def getSelections(self):
+        returnSelections = []
+        selected = self.m_listCtrl1.GetNextItem(-1, state = wx.LIST_STATE_SELECTED)
+        while (selected >= 0):
+            returnSelections.append(selected)
+            selected = self.m_listCtrl1.GetNextItem(selected, state = wx.LIST_STATE_SELECTED)
+        return returnSelections
+
+    def resizeCols(self):
+        if (self.m_listCtrl1.GetItemCount() > 0):
+            self.m_listCtrl1.SetColumnWidth(0, wx.LIST_AUTOSIZE)
+            self.m_listCtrl1.SetColumnWidth(1, wx.LIST_AUTOSIZE)
+            self.m_listCtrl1.SetColumnWidth(2, wx.LIST_AUTOSIZE_USEHEADER)
+            self.m_listCtrl1.SetColumnWidth(3, wx.LIST_AUTOSIZE_USEHEADER)
+        else:
+            self.m_listCtrl1.SetColumnWidth(0, wx.LIST_AUTOSIZE_USEHEADER)
+            self.m_listCtrl1.SetColumnWidth(1, wx.LIST_AUTOSIZE_USEHEADER)
+            self.m_listCtrl1.SetColumnWidth(2, wx.LIST_AUTOSIZE_USEHEADER)
+            self.m_listCtrl1.SetColumnWidth(3, wx.LIST_AUTOSIZE_USEHEADER)
 
 class Gui(wx.App):
     def OnInit(self):
@@ -160,8 +269,5 @@ class Gui(wx.App):
         self.SetTopWindow(self.m_frame)
         return True
 
-    
-
-print 'starting gui...'
 app = Gui(0)
 app.MainLoop()
