2ebaeb0dfe883279dc84a2368f35b3cc4dbcddce
Jeff <Jeff@jeffDesktop>
2010-04-11 10:06:40 -0400

    Added chapter support, guess at volume and chapter numbers from filename.

--- FileJob.py
+++ FileJob.py
@@ -2,23 +2,60 @@ __author__="Jeff Byrom"
 __date__ ="$Apr 2, 2010 8:54:00 AM$"
 
 import os
+import re
+#from decimal import *
 
 class FileJob:
 
     filename = ''
     seriesName = ''
     volume = ''
+    ch = ''
+    myRE = re.compile('\d+')
 
     def __init__(self, filename):
         self.filename = filename
         names = os.path.split(filename)
         self.seriesName = names[0][names[0].rindex('\\') + 1:len(names[0])]
+        #print self.seriesName
+
+        print names[1]
+        try:
+            extIndex = names[1].rindex ('.')
+            try:
+                vIndex = names[1].lower().rindex('v')
+
+                volSection = names[1][vIndex:extIndex]
+                #print volSection
+                volSearch = self.myRE.search(volSection)
+                self.volume = str(int(volSection[volSearch.start():volSearch.end()]))
+                #print 'Volume: ', self.volume
+            except ValueError:
+                self.volume = ''
+
+            try:
+                chIndex = names[1].lower().rindex('c')
+                #if (chIndex > vIndex and chIndex < extIndex):
+                chSection = names[1][chIndex:extIndex]
+                #print chSection
+                chSearch = self.myRE.search(chSection)
+                self.ch = str(int(chSection[chSearch.start():chSearch.end()]))
+                #self.volume += '.' + ch
+                #print 'Chapter: ', self.ch
+            except ValueError:
+                self.ch = ''
+        except ValueError:
+            self.volume = ''
+            self.ch = ''
 
     def setSeriesName(self, seriesName):
         self.seriesName = seriesName
 
     def setVolume(self, volume):
-        self.volume = int(volume)
+        self.volume = str(int(volume))
+
+    def setChapter(self, chapter):
+        self.ch = str(int(chapter))
 
 
     def getFilename(self):
@@ -29,3 +66,6 @@ class FileJob:
 
     def getVolume(self):
         return self.volume
+
+    def getChapter(self):
+        return self.ch
