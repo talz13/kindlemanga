49ef303507c443bdf12058b12c968d9add4e565e
Jeff Byrom <talz13@gmail.com>
2010-04-24 13:23:59 -0400

    setup.py:   Changed dist_dir build directories:     ./dist/kindlemanga_none     ./dist/kindlemanga_dep     ./dist/kindlemanga

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from distutils.core import setup
 import py2exe
 
 data_files=[('.', glob.glob(sys.prefix + '/Lib/site-packages/UnRAR2/UnRARDLL/unrar.dll'))]
-options_bundle_none = dict(bundle_files = 3, dist_dir = './dist_none')
-options_bundle_depends = dict(bundle_files = 2, dist_dir = './dist_dep')
-options_bundle_python = dict(bundle_files = 1, dist_dir = './dist_full')
+options_bundle_none = dict(bundle_files = 3, dist_dir = './dist/kindlemanga_none')
+options_bundle_depends = dict(bundle_files = 2, dist_dir = './dist/kindlemanga_dep')
+options_bundle_python = dict(bundle_files = 1, dist_dir = './dist/kindlemanga')
 
 setup(script_args=['py2exe'], windows=['kindlemanga.py'], data_files=data_files, options = {'py2exe':options_bundle_none})
 setup(script_args=['py2exe'], windows=['kindlemanga.py'], data_files=data_files, options = {'py2exe':options_bundle_depends})
